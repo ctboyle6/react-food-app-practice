d373029e3b5751738e1f5f360e94b7cfbe8cd7f4
Chris Boyle <cthboyle6@gmail.com>
2022-01-19 11:24:09 -0500

    initial build for MealSummary component

---
+++ src/components/Meals/MealsSummary.js
@@ -0,0 +1,28 @@
+import classes from './MealsSummary.module.css';
+
+const MealsSummary = () => {
+  return (
+    <section className={classes.summary}>
+      <h2>Delicious Food, Delivered To You</h2>
+      <p>
+        Red pepper overflowing berries habanero golden sparkling pomegranate
+        punch thyme orange earl grey latte tempeh Indian spiced grapefruit rich
+        coconut cream morning smoothie bowl sweet potato red curry tofu noodles
+        Italian linguine puttanesca lemon cranberry spritzer shallots Thai pesto
+        cool off hemp seeds bite sized appetizer basmati roasted peanuts tabasco
+        pepper bruschetta.
+      </p>
+      <p>
+        A delicious meal street style Thai basil tacos cool creamiest red amazon
+        pepper peanut butter cilantro asian pear kale arugula salad golden
+        cayenne pepper. Caribbean red habanero plums strawberry spinach salad
+        pumpkin artichoke hearts burritos figs soy milk samosa tofu apricot
+        apples shaved almonds overflowing delightful blueberry scones main
+        course cashew cinnamon couscous salad portobello mushrooms banana bread
+        chocolate.
+      </p>
+    </section>
+  );
+};
+
+export default MealsSummary;
